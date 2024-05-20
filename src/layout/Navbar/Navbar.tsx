b9f34b1d8d6bdf18eb4a47dee883990e5cbdc87a
Claire Song <claireyuan.song@gmail.com>
2024-05-20 16:19:04 +0200

    navbar functionalities

--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -1,23 +1,46 @@
 import useSettings from "../../context/useSettings";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 // style
 import "./Navbar.css";
 import { HiMoon, HiSun } from "react-icons/hi";
 const Navbar = () => {
   const { darkMode, setDarkMode } = useSettings();
+  const [currLocation, setCurrLocation] = useState<string>("");
+
+  useEffect(() => {
+    setCurrLocation(location.pathname.split("/")[1]);
+  }, []);
+
+  const navigate = useNavigate();
+
+  function changeLocation(destination: string) {
+    navigate(`/${destination}`);
+    setCurrLocation(destination);
+  }
 
   function handleDarkMode() {
     setDarkMode(!darkMode);
   }
+
   //TODO make navbar appear and disappear en fonction du scroll
   return (
     <nav>
       <div>Claire Song</div>
       <div className="pages">
         <ul>
-          <li>About me</li>
-          <li>Projects</li>
-          <li>Experiences</li>
+          <li className={currLocation === "about" ? "featured" : ""}>
+            <button onClick={() => changeLocation("about")}>About me</button>
+          </li>
+          <li className={currLocation === "projects" ? "featured" : ""}>
+            <button onClick={() => changeLocation("projects")}>Projects</button>
+          </li>
+          <li className={currLocation === "experiences" ? "featured" : ""}>
+            <button onClick={() => changeLocation("experiences")}>
+              Experiences
+            </button>
+          </li>
         </ul>
       </div>
       <div className="settings">
