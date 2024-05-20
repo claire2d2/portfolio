04d0ec3bdb92533ac534bedde37965140991204e
Claire Song <claireyuan.song@gmail.com>
2024-05-20 23:17:39 +0200

    dark mode logic

--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -35,8 +35,36 @@ const Navbar = () => {
     setCurrLocation(destination);
   }
 
+  // handle dark mode
+  // function to set a given theme/color-scheme
+  function setTheme(themeName: string) {
+    localStorage.setItem("theme", themeName);
+    document.documentElement.className = themeName;
+  }
+
+  // function to toggle between light and dark theme
+  function toggleTheme() {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setTheme("theme-light");
+      setDarkMode(false);
+    } else {
+      setTheme("theme-dark");
+      setDarkMode(true);
+    }
+  }
+
+  // Immediately invoked function to set the theme on initial load
+  (function () {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setTheme("theme-dark");
+    } else {
+      setTheme("theme-light");
+    }
+  })();
+
   function handleDarkMode() {
     setDarkMode(!darkMode);
+    toggleTheme();
   }
 
   //TODO make navbar appear and disappear en fonction du scroll
