3fdf8eeedf28132a47f859665c41fa3ae1cc6352
Claire Song <claireyuan.song@gmail.com>
2024-05-23 11:33:58 +0200

    about section init

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 // import { useNavigate } from "react-router-dom";
 
+import About from "../components/About/About";
+
 import "./Homepage.css";
 import meImg from "../assets/images/me.jpeg";
 import { HiCode, HiExternalLink } from "react-icons/hi";
@@ -54,35 +56,7 @@ const Homepage = () => {
           </div>
         </div>
       </section>
-      <section className="part">
-        <h2>About me :</h2>
-        <div className="part-content">
-          <div>
-            <p>
-              I started my career <span>5 years ago</span> as a Recruitment
-              Manager for tech profiles before moving on to Project Manager
-              roles, focusing on digital transformation. During this time, I was
-              simultaneously <span>happy to learn new things</span>, but also
-              increasingly frustrated with not deeply understanding the
-              technical issues I was working with, delegating that task to other
-              people.
-            </p>
-            <p>
-              This led me to pursue a{" "}
-              <span>certification in Web Development with Ironhack</span>, where
-              I graduated top of my class. I now wish to apply my newfound
-              knowledge in a fast-paced environment, where I can keep on
-              learning, and hopefully one day, create opportunities for other
-              people to learn from me as well. My previous experiences have
-              proven me to be a <span>fast learner</span>, to be able to adapt
-              very quickly to new environments and always make my colleagues{" "}
-              <span>smile</span>! I am confident that I have the skills and
-              thirst to learn that will enable me to thrive in this new stage of
-              my professional career.
-            </p>
-          </div>
-        </div>
-      </section>
+      <About />
       <section className="part">
         <h2>Skills:</h2>
         <div className="part-content">
