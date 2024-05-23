fd02b6c27e69e2d826d053f3a2bddbedd300f573
Claire Song <claireyuan.song@gmail.com>
2024-05-23 19:53:06 +0200

    show or hide all projects

--- src/components/Projects/Projects/Projects.tsx
+++ src/components/Projects/Projects/Projects.tsx
@@ -1,4 +1,4 @@
-import { HashLink } from "react-router-hash-link";
+import { useState } from "react";
 
 import allProjects from "../../../data/projects.json";
 import FeaturedProjects from "../FeaturedProjects/FeaturedProjects";
@@ -6,26 +6,36 @@ import OneProject from "../OneProject/OneProject";
 import "./Projects.css";
 
 const Projects = () => {
+  const [showProjects, setShowProjects] = useState<boolean>(false);
   return (
     <div className="Projects">
       <h2>Some of my projects:</h2>
       <FeaturedProjects />
-      <div className="allProjects">
-        {allProjects.map((project) => {
-          return (
-            <OneProject
-              name={project.name}
-              date={project.date}
-              description={project.description}
-              labels={project.labels}
-              codeLink={project.code}
-              link={project.link}
-            />
-          );
-        })}
-      </div>
-      <div>
-        <HashLink to="/#projects">Back to homepage</HashLink>
+
+      <div className="all-projects">
+        <div
+          className="show-projects-button"
+          onClick={() => setShowProjects(!showProjects)}
+        >
+          <div>See all my projects</div>
+          <div>{showProjects ? "-" : "+"}</div>
+        </div>
+        {showProjects && (
+          <div className="show-projects">
+            {allProjects.map((project) => {
+              return (
+                <OneProject
+                  name={project.name}
+                  date={project.date}
+                  description={project.description}
+                  labels={project.labels}
+                  codeLink={project.code}
+                  link={project.link}
+                />
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
